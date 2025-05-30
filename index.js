f9a5aa6fb4566634f02e9d1b259706e2fbfba407
Lam Thanh Nhan <hello@lamnhan.dev>
2025-05-30 07:46:01 +0700

    Refactor project structure and update package metadata for RophimTV app

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  id: "rophim",
-  name: "Rophim",
-  description: "Xem phim trực tuyến từ rophim.me",
-  run: async ({ open }) => {
-    await open("https://rophim.me");
-  },
-};
